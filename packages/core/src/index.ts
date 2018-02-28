31dd756d83e7d7d52cef4c4f85c18ae12bc09f08
Łukasz Kużyński <lukasz.kuzynski@businesshub.com>
2018-02-28 12:14:54 +0100

    v0.1.7

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,15 +4,15 @@ import * as normalizations from './normalizations';
 import {Serializer} from "./Serializer";
 import {JSONAdapter} from "./JSONAdapter";
 
-export const globalNormalizer = new StandardNormalizer();
-
-Serializable.useDataNormalizer(globalNormalizer);
-
 export * from './DataNormalizer';
 export * from './Normalization';
-
+export * from './Serializer';
+export * from './JSONAdapter';
 
 export {Serializable, StandardNormalizer, normalizations};
+
+export const globalNormalizer = new StandardNormalizer();
+Serializable.useDataNormalizer(globalNormalizer);
 
 export const serializer = new Serializer(globalNormalizer, new JSONAdapter());
 export const serialize = serializer.serialize.bind(serializer);
